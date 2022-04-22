788126d23711b19a8deed9e145c3f6503bce74c6
Male <maleaborgna@gmmail.com>
2022-04-22 19:14:06 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { BotonDescargarComponent } from './boton-descargar/boton-descargar.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { ExperienciasComponent } from './experiencias/experiencias.component';
+import { HeaderComponent } from './header/header.component';
+import { SkillsComponent } from './skills/skills.component';
+import { LoginComponent } from './login/login.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { SocialBarComponent } from './social-bar/social-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercaDeComponent,
+    BotonDescargarComponent,
+    EducacionComponent,
+    ExperienciasComponent,
+    HeaderComponent,
+    SkillsComponent,
+    LoginComponent,
+    ProyectosComponent,
+    SocialBarComponent
   ],
   imports: [
     BrowserModule,
